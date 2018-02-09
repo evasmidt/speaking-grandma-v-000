d4dadc3507ed7175b64ef7042874464b9956cbcd
Eva Smidt <shevangeline@gmail.com>
2018-02-09 03:15:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -22,11 +22,11 @@
 #end
 
 
-def speak_to_grandma (phrase)
-  if phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-   return  "I LOVE YOU TOO PUMPKIN!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+   return  "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
